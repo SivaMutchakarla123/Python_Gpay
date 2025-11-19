d3faa421772175cf8b87b366d4abf4c1e8294948
MutchakarlaSiva <164531076+SivaMutchakarla123@users.noreply.github.com>
2025-11-19 22:49:49 +0530

    Delete Transaction.py

--- Transaction.py
+++
@@ -1,7 +0,0 @@
-class Transaction:
-    def __init__(self):
-        self.amountSent = 0 
-        self.senderName = None 
-        self.receiverName = None 
-        self.senderBankName = None 
-        self.receiverBankName = None 
