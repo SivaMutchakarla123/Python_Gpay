b159d05fa2c987d1b26d5bcbc509bc7624783946
MutchakarlaSiva <164531076+SivaMutchakarla123@users.noreply.github.com>
2025-11-19 22:50:01 +0530

    Delete User.py

--- User.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self):
-        self.fullName = None 
-        self.mobileNumber = None 
-        self.pin = None 
-        self.defaultBankAccount = None
-        self.previousTransactions = []
-        self.linkedBankAccounts = []
